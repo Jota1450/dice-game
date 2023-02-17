ee012401fb6eed61bd89b0342b8e26ff0deeccbc
Jorge Torres <54687758+Jota1450@users.noreply.github.com>
2023-02-17 01:15:31 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import Dices from './components/Dices.vue';
+</script>
+
+<template>
+  <div class="container is-max-desktop">
+    <section class="section">
+      <div class="card">
+        <div class="card-header">
+          <div class="card-header-title">
+            Dice Game
+          </div>
+        </div>
+        <div class="card-content">
+          <div class="content">
+            <Dices />
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<style scoped>
+@import "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css";
+</style>
---
+++ src/components/Dices.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="custom-container">
+        <button class="button is-link" v-on:click="throwDice">
+            Tirar
+        </button>
+    </div>
+    <div class="columns ">
+        <div class="column is-flex is-justify-content-center">
+            <figure class="image custom-background">
+                <img v-bind:src="leftDice" alt="Left Dice">
+            </figure>
+        </div>
+        <div class="column is-flex is-justify-content-center">
+            <figure class="image custom-background">
+                <img v-bind:src="rigthDice" alt="Rigth Dice">
+            </figure>
+        </div>
+    </div>
+
+    <article v-if="leftDice === rigthDice" class="message is-success is-small">
+        <div class="message-header">
+            <p>Congrats</p>
+        </div>
+        <div class="message-body">
+            You Win!!!!. Wanna play again?😏
+        </div>
+    </article>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            leftDice: 'assets/img/dice1.png',
+            rigthDice: 'assets/img/dice6.png',
+        }
+    },
+    methods: {
+        throwDice() {
+            this.leftDice = `assets/img/dice${this.getRandomNumber()}.png`
+            this.rigthDice = `assets/img/dice${this.getRandomNumber()}.png`
+        },
+        getRandomNumber(min = 1, max = 6) {
+            min = Math.ceil(min);
+            max = Math.floor(max);
+            return Math.floor(Math.random() * (max - min) + min);
+        }
+    },
+}
+</script>
+
+<style scoped>
+.custom-background {
+    background: grey;
+    border-radius: 20px;
+}
+
+.custom-container {
+    display: flex;
+    justify-content: center;
+}
+</style>
